5daa444e3876c617cdba1a13a363c514deb80239
yengni <yeeeeeun1013@naver.com>
2024-10-07 16:37:27 +0900

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+/* $('.cont5 .contin .thumnail').on('click', function(event){
+    event.preventDefault();//a태그의 href기능을 막음
+    $('.popup').show();
+});
+$('.popup').on('click', function(e){
+    e.preventDefault();
+    $('.popup').hide();
+}); */
+
+const thumnail_detail = document.querySelectorAll('#detailDesign .thumnail a')
+const popup_detail = document.querySelector('#detailDesign .popup')
+console.log(popup_detail)
+
+for(i of thumnail_detail){
+    i.addEventListener('click',function(e){
+        e.preventDefault();
+        console.log(this.children[0].src)
+        popup_detail.style.display = 'block'
+        popup_detail.children[0].src = this.children[0].src
+    })
+}
+
+popup_detail.addEventListener('click',()=>{
+    popup_detail.style.display = 'none'
+})
